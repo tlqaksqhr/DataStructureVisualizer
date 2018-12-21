dcd77e169eb05c6aaa87f9741f56be06994211cb
semtax <semtax@hanyang.ac.kr>
2018-12-21 22:39:54 +0900

    implement stack

---
+++ datastructure.js
@@ -0,0 +1,29 @@
+class Stack{
+
+	constructor(){
+		this.container = [];
+	}
+
+	push(element){
+		this.container.push(element);
+	}
+
+	pop(){
+		this.container.pop();
+	}
+
+	top(){
+		return this.container[this.container.length-1];
+	}
+
+	size(){
+		return this.container.length;
+	}
+
+	empty(){
+		if(this.container.length === 0)
+			return true;
+		else
+			return false;
+	}
+}
